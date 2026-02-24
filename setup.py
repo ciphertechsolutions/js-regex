0790176ba4f557f1ca92e493be4cf3dc2f6cfb5d
rhartig-ct <rhartig@ciphertechsolutions.com>
2026-02-24 15:02:14 -0700

    Change supported versions to 3.10+

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setuptools.setup(
             "pytest-cov",
         ]
     },
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    python_requires=">=3.10",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
